670bc6723cdd2f2352997902eae2de1fa5e39948
Steven Ward <stevenward94@gmail.com>
2016-07-24 21:48:09 -0500

    Have to restart Practice.hs...sigh

--- Practice.hs
+++ Practice.hs
@@ -16,16 +16,57 @@
 -- modules, etc.
 --
 
-data Vector = Empty | Vector [a] deriving (Show)
+import Prelude hiding( length, (+), empty, (*) )
+import qualified Prelude as P
 
--- | get the length of a vector
+infixr 5 :+:
+data Vector a = Empty | Vec [a] | a :+: (Vector a) deriving (Show)
+
+-- | zero and unit are inifinite-length representations of the zero- and
+-- unit-vectors, respectively. zero' and unit' are functions that generate
+-- finite-length zero- and unit-vectors, respectively.
+zero :: Vector Int
+zero = Vec (repeat 0)
+
+unit :: Vector Int
+unit = Vec (repeat 1)
+
+zero' :: (Integral a) => a -> Vector Int
+zero' n = Vec (take n zero)
+
+unit' :: (Integral a) => a -> Vector Int
+unit' n = Vec (take n unit)
+
+-- | Getting the length/size (# of elements) of a Vector
 length :: Vector a -> Int
 length Empty = 0
-length (Vector (x:xs)) = 1 + length xs
-
+length zero  = error "Error: zero is an infinite size - try zero' for a finite zero-vector"
+length unit  = error "Error: unit is an infinite size - try unit' for a finite unit-vector"
+length zero' = id
+length unit' = id
+length (Vec (x:xs)) = 1 P.+ length (Vec xs)
 size = length
 
--- | check if a vector is empty
+-- | Determine whether a Vector is Empty (calls length, so zero & unit will
+-- throw errors
 empty :: Vector a -> Bool
-empty Empty = True
-empty (Vector [a]) = False
+empty v = length v == 0
+
+-- | Vector addition
+infixl 6 +
+(+) :: (Num a) => Vector a -> Vector a -> Vector a
+(+) v Empty = v
+(+) Empty = id
+(+) v zero = v
+(+) zero = id
+(+) (Vec (x:xs)) unit = Vec (x P.+ 1 : xs + unit)
+(+) (Vec (x:xs)) (Vec (y:ys)) = Vec (x P.+ y : xs + ys)
+
+-- | Scalar multiplication with a vector (the 'dot' product)
+infixl 7 *
+(*) :: (Num a) => Int -> Vector a -> Vector a
+(*) _ Empty = Empty
+(*) _ zero = zero
+(*) 0 _ = zero
+(*) 1 = id
+(*) c (Vec (x:xs)) = Vec (c P.* x : c * xs)
