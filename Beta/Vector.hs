7522bd6948bd7a7183307c5c7a3dea34d7e0f4ab
Steven Ward <stevenward94@gmail.com>
2016-07-27 13:07:19 -0500

    Complete Vector instance of Show typeclass

--- Beta/Vector.hs
+++ Beta/Vector.hs
@@ -2,14 +2,19 @@
 -- Module:        Beta.Vector
 -- Author:        Steven Ward <stevenward94@gmail.com>
 -- URL:           https://github.com/StevenWard94/mathskell
--- Last Change:   2016 July 25
+-- Last Change:   2016 July 27
 --
 
 data Vector = Zero | Unit | Vector [Int]
-type Empty = Vector []
 
 instance Show Vector where
-  show Empty = "( )"
   show Zero = "{ 0 }"
   show Unit = "{ 1 }"
-  show (Vector (x:xs)) = "( " ++ show x ++ " " ++ show (Vector xs) ++ " )"
+  show (Vector []) = ""
+  show (Vector [x]) = "( " ++ show x ++ " )"
+  show (Vector (x:xs)) = "( " ++ show x ++ " " ++ showOthers xs ++ " )"
+                      where
+                        showOthers ys = case ys of
+                            []      -> ""
+                            [y]     -> show y
+                            (y:ys)  -> show y ++ " " ++ showOthers ys
