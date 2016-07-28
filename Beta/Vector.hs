ddb6f2d66aba12c7d0a2931e7d261218209f88ee
Steven Ward <stevenward94@gmail.com>
2016-07-27 23:01:58 -0500

    Add definitions for Vector instance of Ord & Read. Add definition of length :: Vector -> Int

--- Beta/Vector.hs
+++ Beta/Vector.hs
@@ -5,6 +5,13 @@
 -- Last Change:   2016 July 27
 --
 
+import Prelude hiding( length, (*), (+) )
+import qualified Prelude as P
+import Data.Function
+import Data.Char ( isDigit, isSpace )
+import Data.List ( nub )
+import Data.List.Split ( splitWhen )
+
 data Vector = Zero | Unit | Vector [Int]
 
 instance Show Vector where
@@ -19,6 +26,15 @@ instance Show Vector where
                             [y]     -> show y
                             (y:ys)  -> show y ++ " " ++ showOthers ys
 
+instance Read Vector where
+  readsPrec _ input
+    | P.null result = [(Vector [], "")]
+    | nub result == [0] = [(Zero,"")]
+    | nub result == [1] = [(Unit,"")]
+    | otherwise = [(Vector result, "")]
+    where
+      result = map (read :: String -> Int) (filter (not . P.null) (splitWhen isSpace (filter (\c -> isDigit c || isSpace c) input)))
+
 instance Eq Vector where
   u == v = case (u,v) of
              (Zero,Zero) -> True
@@ -28,10 +44,20 @@ instance Eq Vector where
              (Vector (x:xs), Vector (y:ys))
                                           | length u /= length v -> False
                                           | otherwise -> (x == y) && (Vector xs == Vector ys)
+             _ -> False
+
+instance Ord Vector where
+  u `compare` v = compare (length u) (length v)
 
 null :: Vector -> Bool
 null v = case v of
             (Vector []) -> True
             _           -> False
 
-
+length :: Vector -> Int
+length Zero = error "{ 0 } is of indefinite length"
+length Unit = error "{ 1 } is of indefinite length"
+length v = case v of
+               Vector []     -> 0
+               Vector [x]    -> 1
+               Vector (x:xs) -> 1 P.+ length (Vector xs)
