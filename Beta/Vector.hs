a42de5d63b33966fe5c5baabc05096e243ca6d95
Steven Ward <stevenward94@gmail.com>
2016-08-20 17:31:20 -0500

    More work on data type instances for Vector in Beta.Vector

--- Beta/Vector.hs
+++ Beta/Vector.hs
@@ -2,11 +2,12 @@
 -- Module:        Beta.Vector
 -- Author:        Steven Ward <stevenward94@gmail.com>
 -- URL:           https://github.com/StevenWard94/mathskell
--- Last Change:   2016 Aug 19
+-- Last Change:   2016 Aug 20
 --
 
 module Beta.Vector where
 
+import Control.Monad
 import Control.Applicative
 
 data Vector a = Vector | a :. (Vector a)
@@ -14,7 +15,7 @@ infixr 5 :.
 
 instance Show a => Show (Vector a) where
     show Vector       =  "( ø )"
-    show (v:.Vector)  =  "( v )"
+    show (v:.Vector)  =  "( " ++ show v ++ " )"
     show (v:.vs)      =  "( " ++ show v ++ ", " ++ showRest vs
         where showRest vs = case vs of
                                 (v:.Vector) -> show v ++ " )"
@@ -33,12 +34,30 @@ instance Functor Vector where
     fmap = (fromList .) . (. toList) . (<$>)
 
 instance Applicative Vector where
-    pure  = (:. Vector)
+    pure  = return
     vecVals  <*>  vecFuncs  =  fromList (zipWith ($) (toList vecVals) (toList vecFuncs))
+
+instance Monad Vector where
+    return  =  (:. Vector)
+    (>>=)  =  (joinV .) . flip fmap
 
 instance Monoid (Vector a) where
     mempty  =  Vector
     mappend = (++.)
+
+instance Alternative Vector where
+    empty  =  Vector
+    (<|>)  =  (++.)
+
+instance MonadPlus Vector where
+    mzero  =  mempty
+    mplus  =  mappend
+
+instance Foldable Vector where
+    foldr k z  =  go
+        where
+            go Vector  =  z
+            go (v:.vs) =  v `k` go vs
 
 (++.) :: Vector a -> Vector a -> Vector a
 (++.)  =  (fromList .) . (. toList) . (++) . toList
@@ -53,3 +72,7 @@ fromList :: [a] -> Vector a
 fromList []     = Vector
 fromList [x]    = x:.Vector
 fromList (x:xs) = x :. fromList xs
+
+joinV :: Vector (Vector a) -> Vector a
+join Vector  =  Vector
+joinV (vs:.vss)  =  vs ++. joinV vss
